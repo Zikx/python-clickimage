5511d49e1be898be6ce22af1c38b17c522d53c06
Zikx <8687706@gmail.com>
2020-10-14 15:00:45 +0900

    Add findpos

--- run.py
+++ run.py
@@ -2,11 +2,14 @@ import sys
 import pyautogui
 import time
 import random
+import winsound
+# recom x=756, y=1019
 
 def main(command,*args):
     if 'click' in command:
+        x, y = findpos()
         while True:
-            pyautogui.click(pyautogui.position(),button='left',clicks=1)
+            pyautogui.click(x, y,button='left',clicks=1)
             t = random.randrange(int(command[command.index('click') + 1]),int(command[command.index('click') + 2]))
             print(f'after {t} seconds will be refreshed')
             time.sleep(t)
@@ -17,8 +20,13 @@ def main(command,*args):
                 pyautogui.doubleClick(params)
         except:
             pass
-        
-    
+
+def findpos():
+    tar_time = time.time() + 3
+    while True: 
+        if time.time() > tar_time:
+            winsound.Beep(440, 250)
+            return pyautogui.position()
 
 if __name__ == "__main__":
     main(sys.argv)
