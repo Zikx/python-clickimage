fbe855571a136003e64e33219f7243f618625969
Zikx <8687706@gmail.com>
2020-10-14 15:23:00 +0900

    Add move random pos in list

--- run.py
+++ run.py
@@ -3,13 +3,16 @@ import pyautogui
 import time
 import random
 import winsound
+
 # recom x=756, y=1019
 
 def main(command,*args):
     if 'click' in command:
-        x, y = findpos()
+        n = int(input('Set the number of position : '))
+        loca = [findpos() for _ in range(n)]
+
         while True:
-            pyautogui.click(x, y,button='left',clicks=1)
+            pyautogui.click(random.choice(loca),button='left',clicks=1)
             t = random.randrange(int(command[command.index('click') + 1]),int(command[command.index('click') + 2]))
             print(f'after {t} seconds will be refreshed')
             time.sleep(t)
@@ -22,11 +25,12 @@ def main(command,*args):
             pass
 
 def findpos():
-    tar_time = time.time() + 3
+    tar_time = time.time() + 2
     while True: 
         if time.time() > tar_time:
             winsound.Beep(440, 250)
-            return pyautogui.position()
+            x, y = pyautogui.position()
+            return x, y
 
 if __name__ == "__main__":
     main(sys.argv)
