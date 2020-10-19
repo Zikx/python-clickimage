17797560d124b0e869ea12694f9eb672462da8b9
Zikx <8687706@gmail.com>
2020-10-19 17:54:44 +0900

    Add Random move mouse

--- run.py
+++ run.py
@@ -7,15 +7,21 @@ import winsound
 # recom x=756, y=1019
 
 def main(command,*args):
+    n = int(input('Set the number of position : '))
+    loca = [findpos() for _ in range(n)]
+    
     if 'click' in command:
-        n = int(input('Set the number of position : '))
-        loca = [findpos() for _ in range(n)]
-
         while True:
             pyautogui.click(random.choice(loca),button='left',clicks=1)
             t = random.randrange(int(command[command.index('click') + 1]),int(command[command.index('click') + 2]))
             print(f'after {t} seconds will be refreshed')
             time.sleep(t)
+
+    elif 'move' in command:
+        while True:
+            t = random.randrange(int(command[command.index('move') + 1]),int(command[command.index('move') + 2]))
+            moveMouse(random.choice(loca),t)
+            # time.sleep(t)
     else:
         try:
             while True:
@@ -31,6 +37,10 @@ def findpos():
             winsound.Beep(440, 250)
             x, y = pyautogui.position()
             return x, y
+
+def moveMouse(pos, duration):
+    x, y = pos
+    pyautogui.moveTo(x,y,duration)
 
 if __name__ == "__main__":
     main(sys.argv)
