2a0624d189ac1c5bcf8c2abc24a4b091855e9347
zikx <8687706@gmail.com>
2020-12-08 11:21:19 +0900

    Add auto

--- run.py
+++ run.py
@@ -9,6 +9,15 @@ import schedule
 
 def main():
     command = input('Please enter a command : ')
+
+    loca_fixed = [(141, 138), (106, 490), (115, 531), (355, 300), (508, 311)]
+    if 'auto' in command:
+        while True:
+            pyautogui.click(random.choice(loca_fixed),button='left',clicks=1)
+            t = random.randrange(120, 400)
+            print(f'after {t} seconds will be refreshed')
+            time.sleep(t)
+
     x, y = map(int, input('Please enter the time interval : ').split())
     n = int(input('Set the number of position : '))
     loca = [findpos() for _ in range(n)]
@@ -20,7 +29,6 @@ def main():
             t = random.randrange(x, y)
             print(f'after {t} seconds will be refreshed')
             time.sleep(t)
-
 
     elif 'move' in command:
         while True:
